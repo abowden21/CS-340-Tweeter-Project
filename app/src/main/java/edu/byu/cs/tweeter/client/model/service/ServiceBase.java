9bf91cacf969f374fe7132bd328ea0b99895d1a8
John Simmons <john.l.simmons31@gmail.com>
2021-03-17 18:51:20 -0600

    Added support for uploading images to S3

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/ServiceBase.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/ServiceBase.java
@@ -7,15 +7,11 @@ import androidx.annotation.RequiresApi;
 import edu.byu.cs.tweeter.client.model.net.ServerFacade;
 
 public class ServiceBase {
-    /**
-     * Returns an instance of {@link ServerFacade}. Allows mocking of the ServerFacade class for
-     * testing purposes. All usages of ServerFacade should get their ServerFacade instance from this
-     * method to allow for proper mocking.
-     *
-     * @return the instance.
-     */
+    private ServerFacade serverFacade;
     @RequiresApi(api = Build.VERSION_CODES.O)
     ServerFacade getServerFacade() {
-        return new ServerFacade();
+        if (serverFacade == null)
+            serverFacade = new ServerFacade();
+        return serverFacade;
     }
 }
--- server/src/main/java/edu/byu/cs/tweeter/server/service/LoginServiceImpl.java
+++ server/src/main/java/edu/byu/cs/tweeter/server/service/LoginServiceImpl.java
@@ -1,6 +1,24 @@
 package edu.byu.cs.tweeter.server.service;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.S3BucketResource;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectIdBuilder;
+import com.amazonaws.services.s3.model.UploadObjectRequest;
+import com.amazonaws.util.Base64;
+
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import edu.byu.cs.tweeter.server.dao.AuthTokenDAO;
 import edu.byu.cs.tweeter.server.dao.UserDAO;
@@ -32,6 +50,19 @@ public class LoginServiceImpl implements LoginServiceInterface {
     public RegisterResponse register(RegisterRequest registerRequest) {
         // Receive image bytes as B64 string, upload to S3, and return the URL
         String imageUrl = "https://faculty.cs.byu.edu/~jwilkerson/cs340/tweeter/images/donald_duck.png";
+        if (!registerRequest.getImageBase64String().isEmpty()) {
+            String bucketName = "jlsim-cs340-twitter-clone";
+            AmazonS3 s3 = AmazonS3Client.builder().withRegion("us-east-1").build();
+            String imgKey = registerRequest.getUsername() + "_" + LocalDateTime.now().toString() + ".jpg";
+            byte[] imageBytes = Base64.decode(registerRequest.getImageBase64String());
+            ByteArrayInputStream imageStream = new ByteArrayInputStream(imageBytes);
+            PutObjectRequest s3Request =
+                    new PutObjectRequest(bucketName, imgKey, imageStream, new ObjectMetadata())
+                    .withCannedAcl(CannedAccessControlList.PublicRead);
+            PutObjectResult s3Result = s3.putObject(s3Request);
+            URL imageUrlObject = s3.getUrl(bucketName, imgKey);
+            imageUrl = imageUrlObject.toString();
+        }
         //
         User user = new User(registerRequest.getFirstName(), registerRequest.getLastName(),
                 registerRequest.getUsername(), imageUrl);
