9853b4843c617ef6689399031c143410e51822de
ryanb <rb593@byu.edu>
2021-03-19 02:20:51 -0600

    Updated original story tests to new methods/class names

--- app/src/test/java/edu/byu/cs/tweeter/client/model/service/StoryServiceTest.java
+++ app/src/test/java/edu/byu/cs/tweeter/client/model/service/StoryServiceTest.java
@@ -12,6 +12,7 @@ import java.util.Arrays;
 import edu.byu.cs.tweeter.shared.model.domain.Status;
 import edu.byu.cs.tweeter.shared.model.domain.User;
 import edu.byu.cs.tweeter.client.model.net.ServerFacade;
+import edu.byu.cs.tweeter.shared.model.net.TweeterRemoteException;
 import edu.byu.cs.tweeter.shared.model.request.StoryRequest;
 import edu.byu.cs.tweeter.shared.model.response.StoryResponse;
 
@@ -22,38 +23,38 @@ public class StoryServiceTest {
     private StoryResponse successResponse;
     private StoryResponse failureResponse;
 
-    private StoryService storyServiceSpy;
+    private StoryServiceProxy storyServiceSpy;
 
     @BeforeEach
-    public void setup() {
+    public void setup() throws IOException, TweeterRemoteException {
         User user1 = new User("firstname1", "lastname1", "https://faculty.cs.byu.edu/~jwilkerson/cs340/tweeter/images/donald_duck.png");
 
         Status resultStatus1 = new Status(LocalDateTime.parse("2021-02-13T01:01:01"), "message1", user1);
         Status resultStatus2 = new Status(LocalDateTime.parse("2021-02-13T01:01:02"), "message2", user1);
         Status resultStatus3 = new Status(LocalDateTime.parse("2021-02-13T01:01:03"), "message3", user1);
 
-        validRequest = new StoryRequest("@validAlias", 3, null);
-        invalidRequest = new StoryRequest("@invalidAlias", 0, null);
+        validRequest = new StoryRequest("@validAlias", 3, "");
+        invalidRequest = new StoryRequest("@invalidAlias", 0, "");
 
         successResponse = new StoryResponse(Arrays.asList(resultStatus1, resultStatus2, resultStatus3), false);
         ServerFacade mockServerFacade = Mockito.mock(ServerFacade.class);
-        Mockito.when(mockServerFacade.getStory(validRequest)).thenReturn(successResponse);
+        Mockito.when(mockServerFacade.getStory(validRequest, "/view/story")).thenReturn(successResponse);
 
         failureResponse = new StoryResponse("An exception occurred");
-        Mockito.when(mockServerFacade.getStory(invalidRequest)).thenReturn(failureResponse);
+        Mockito.when(mockServerFacade.getStory(invalidRequest, "/view/story")).thenReturn(failureResponse);
 
-        storyServiceSpy = Mockito.spy(new StoryService());
+        storyServiceSpy = Mockito.spy(new StoryServiceProxy());
         Mockito.when(storyServiceSpy.getServerFacade()).thenReturn(mockServerFacade);
     }
 
     @Test
-    public void testGetStory_validRequest_correctResponse() throws IOException {
+    public void testGetStory_validRequest_correctResponse() throws IOException, TweeterRemoteException {
         StoryResponse response = storyServiceSpy.getStory(validRequest);
         Assertions.assertEquals(successResponse, response);
     }
 
     @Test
-    public void testGetStory_validRequest_loadsUsers() throws IOException {
+    public void testGetStory_validRequest_loadsUsers() throws IOException, TweeterRemoteException {
         StoryResponse response = storyServiceSpy.getStory(validRequest);
 
         for (Status status: response.getStatuses()) {
@@ -62,7 +63,7 @@ public class StoryServiceTest {
     }
 
     @Test
-    public void testGetStory_invalidRequest_returnsNoStatuses() throws IOException {
+    public void testGetStory_invalidRequest_returnsNoStatuses() throws IOException, TweeterRemoteException {
         StoryResponse response = storyServiceSpy.getStory(invalidRequest);
         Assertions.assertEquals(failureResponse, response);
     }
--- app/src/test/java/edu/byu/cs/tweeter/client/presenter/StoryPresenterTest.java
+++ app/src/test/java/edu/byu/cs/tweeter/client/presenter/StoryPresenterTest.java
@@ -9,8 +9,10 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 
+import edu.byu.cs.tweeter.client.model.service.StoryServiceProxy;
 import edu.byu.cs.tweeter.shared.model.domain.Status;
 import edu.byu.cs.tweeter.shared.model.domain.User;
+import edu.byu.cs.tweeter.shared.model.net.TweeterRemoteException;
 import edu.byu.cs.tweeter.shared.model.request.StoryRequest;
 import edu.byu.cs.tweeter.shared.model.response.StoryResponse;
 
@@ -18,36 +20,36 @@ public class StoryPresenterTest {
 
     private StoryRequest request;
     private StoryResponse response;
-    private StoryService mockStoryService;
+    private StoryServiceProxy mockStoryService;
     private StoryPresenter presenter;
 
     @BeforeEach
-    public void setup() throws IOException {
+    public void setup() throws IOException, TweeterRemoteException {
         User user1 = new User("firstname1", "lastname1", "https://faculty.cs.byu.edu/~jwilkerson/cs340/tweeter/images/donald_duck.png");
 
         Status resultStatus1 = new Status(LocalDateTime.parse("2021-02-13T01:01:01"), "message1", user1);
         Status resultStatus2 = new Status(LocalDateTime.parse("2021-02-13T01:01:02"), "message2", user1);
         Status resultStatus3 = new Status(LocalDateTime.parse("2021-02-13T01:01:03"), "message3", user1);
 
-        request = new StoryRequest("@validAlias", 4, null);
+        request = new StoryRequest("@validAlias", 4, "");
         response = new StoryResponse(Arrays.asList(resultStatus1, resultStatus2, resultStatus3), false);
 
-        mockStoryService = Mockito.mock(StoryService.class);
+        mockStoryService = Mockito.mock(StoryServiceProxy.class);
         Mockito.when(mockStoryService.getStory(request)).thenReturn(response);
 
         presenter = Mockito.spy(new StoryPresenter(new StoryPresenter.View() {}));
-        Mockito.when(presenter.getStoryService()).thenReturn(mockStoryService);
+        Mockito.when(presenter.getStoryServiceProxy()).thenReturn(mockStoryService);
     }
 
     @Test
-    public void testGetStory_returnsServiceResult() throws IOException {
+    public void testGetStory_returnsServiceResult() throws IOException, TweeterRemoteException {
         Mockito.when(mockStoryService.getStory(request)).thenReturn(response);
 
         Assertions.assertEquals(response, presenter.getStory(request));
     }
 
     @Test
-    public void testGetStory_serviceThrowsIOException_presenterThrowsIOException() throws IOException {
+    public void testGetStory_serviceThrowsIOException_presenterThrowsIOException() throws IOException, TweeterRemoteException {
         Mockito.when(mockStoryService.getStory(request)).thenThrow(new IOException());
 
         Assertions.assertThrows(IOException.class, () -> {
